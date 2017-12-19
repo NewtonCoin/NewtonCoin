f8e1ffa2d7b23ed9492ebe8eb25f35f408451588
NewtonCoin <34669734+NewtonCoin@users.noreply.github.com>
2017-12-19 18:03:26 +1300

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -1,1 +1,6 @@
 const char CRYPTONOTE_NAME[] = "NewtonCoin";
+const uint64_t MONEY_SUPPLY = (uint64_t)(1000);
+const unsigned EMISSION_SPEED_FACTOR = 18;
+const uint64_t DIFFICULTY_TARGET = 120;
+const int P2P_DEFAULT_PORT = 37009;
+const int RPC_DEFAULT_PORT = 37476;
